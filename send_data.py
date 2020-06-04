d90e3cf37fdeb0b8edca92f402e69ea8a207c9e2
Radu <radu98@gmail.com>
2020-06-05 00:35:35 +0200

    added script that reads data and creates data Objects

--- send_data.py
+++ send_data.py
@@ -1,8 +1,45 @@
 import redis
 import json
 import xmltodict
+import re
+import datetime
 from xml.etree import ElementTree
 from collections import OrderedDict
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
+
+class Data:
+    def __init__(self, url, date_time, category, postal_code, string, integer, id_string):
+        self.url = url
+        self.date_time = date_time
+        self.category = category
+        self.postal_code = postal_code
+        self.string = string
+        self.integer = integer
+        self.id_string = id_string
+        self.grade = 'F'
+
+    def validate_url(self):
+        val = URLValidator(verify_exists=False)
+        try:
+            val(self.url)
+        except ValidationError as e:
+            print(e)
+
+    def validate_date_time(self):
+        r = re.compile('^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}')
+        if r.match(self.date_time) is None:
+            print('Not a date and/or time')
+
+    def print_data(self):
+        print(self.url)
+        print(self.date_time)
+        print(self.category)
+        print(self.postal_code)
+        print(self.string)
+        print(self.integer)
+        print(self.id_string)
 
 
 def to_dict(input_ordered_dict):
@@ -29,20 +66,31 @@ def read_json_file(json_file):
 
 
 input_files = ['Sample Input Files/input-sample1.xml', 'Sample Input Files/input-sample3.json']
+
+
 # , 'Sample Input Files/input-sample2.xml']
+
+
+def validate_and_grade(data):
+    url = list(data.values())[0]
+    date_time = list(data.values())[1]
+    category = list(data.values())[2]
+    postal_code = list(data.values())[3]
+    string = list(data.values())[4]
+    integer = list(data.values())[5]
+    id_string = list(data.values())[6]
+    data_item = Data(url, date_time, category, postal_code, string, integer, id_string)
+    data_item.print_data()
 
 
 for file in input_files:
     if file.endswith('.xml'):
         xml_data = read_xml_file(file)
         for i in range(len(xml_data)):
-            print(xml_data[i])
-
+            # print(xml_data[i])
+            validate_and_grade(xml_data[i])
     elif file.endswith('.json'):
         json_data = read_json_file(file)
         for i in range(len(json_data)):
-            print(json_data[i])
-
-# r = redis.StrictRedis()
-# r.execute_command('JSON.SET', 'doc', '.', json.dumps(data))
-# reply = json.loads(r.execute_command('JSON.GET', 'doc'))
+            # print(json_data[i])
+            validate_and_grade(json_data[i])
